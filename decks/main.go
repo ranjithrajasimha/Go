7ce412023b317cbf29a559574087e3969239287f
ranjithrajasimha <ranjithrajasimha@gmail.com>
2020-04-19 19:32:16 +0200

    Add a function with return statement

--- decks/main.go
+++ decks/main.go
@@ -4,7 +4,10 @@ import "fmt"
 
 func main() {
 	// var card string = "Ace of Spades"
-	card := "Ace of Spades"
-	card = "Five of Diamonds"
+	card := NewCard()
 	fmt.Println(card)
+}
+
+func NewCard() string {
+	return "Ace of Spades"
 }
